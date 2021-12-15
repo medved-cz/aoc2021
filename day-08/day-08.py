0288ec74bff07cf6839c7f50dce028e4ab2ca1a9
Jan Bělina <Jan.Belina@cesnet.cz>
2021-12-15 14:10:22 +0100

    day-08 vylepšen

--- day-08/day-08.py
+++ day-08/day-08.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python3
 
+C1 = '\033[92m'
+C0 = '\033[0m'
+
+data = 'input'
+#data = 'input-test'
+
+print()
+print('--- Day 8: Seven Segment Search ---')
+print('Input file is: ' +C1 + str(data) + C0, end = '\n' * 2)
+
+input = open(data, "r")
+
 def sort(string):
     return(''.join(sorted(string)))
-
-input = open("input", "r")
-#input = open("input-test2", "r")
-
-lines = input.readlines()
-numlines = len(lines)
-a1 = [''] * numlines
-a2 = [''] * numlines
-a3 = [''] * numlines
-count = 0
-
-for i in range(numlines):
-    lines[i] = lines[i].replace('\n','')
-    parts = lines[i].split(' | ')
-    a1[i] = parts[0]
-    a2[i] = parts[1]
-    a1[i] = a1[i].split()
-    a2[i] = a2[i].split()
-
-for i in range(numlines):
-    for j in range(len(a2[i])):
-        if len(a2[i][j]) == 2 or len(a2[i][j]) == 3 or len(a2[i][j]) == 4 or len(a2[i][j]) == 7:
-            count += 1
 
 def getdigits(codes):
     codes5 = []
     codes6 = []
     digits = [''] * 10
     for x in codes:
-        if len(x) == 2:
+        if len(x) == 2: # n1
             digits[1] = x
-        elif len(x) == 3:
+        elif len(x) == 3: # n7
             digits[7] = x
-        elif len(x) == 4:
+        elif len(x) == 4: # n4
             digits[4] = x
-        elif len(x) == 7:
+        elif len(x) == 7: #n8
             digits[8] = x
         elif len(x) == 5:
             codes5.append(x)
@@ -68,10 +57,31 @@ def getdigits(codes):
     digits[2] = codes5[0] # n2
     codes5.remove(digits[2])
 
-    digits[0] = codes6[0] # n2
+    digits[0] = codes6[0] # n0
     codes6.remove(digits[0])
 
     return(digits)
+
+lines = input.readlines()
+input.close()
+numlines = len(lines)
+a1 = [''] * numlines
+a2 = [''] * numlines
+a3 = [''] * numlines
+count = 0
+
+for i in range(numlines):
+    lines[i] = lines[i].replace('\n','')
+    parts = lines[i].split(' | ')
+    a1[i] = parts[0]
+    a2[i] = parts[1]
+    a1[i] = a1[i].split()
+    a2[i] = a2[i].split()
+
+for i in range(numlines):
+    for j in range(len(a2[i])):
+        if len(a2[i][j]) == 2 or len(a2[i][j]) == 3 or len(a2[i][j]) == 4 or len(a2[i][j]) == 7:
+            count += 1
 
 for line in range(len(a1)):
     digits = getdigits(a1[line])
@@ -82,8 +92,17 @@ for line in range(len(a1)):
                     a3[line] = str(a3[line])+(str(num))
 
 result = 0
+for a in a2:
+    for b in a:
+        if len(b) == 2 or len(b) == 3 or len(b) == 4 or len(b) == 7:
+            result += 1
+
+print('--- Part One ---')
+print('Digits 1, 4, 7 and 8 apeared ' + C1 + str(result) + C0 + ' times.', end = '\n' * 2)
+
+result = 0
 for x in a3:
     result += int(x)
 
-print(result)
-
+print('--- Part Two ---')
+print('Sum of all values is: ' + C1 + str(result) + C0, end = '\n' * 2)
